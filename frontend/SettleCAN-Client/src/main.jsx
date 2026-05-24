ac7ddadae24234180a076a6b3939fafceed275f3
xeniaBab <xenbabko@gmail.com>
2026-05-24 15:10:26 -0400

    Resolve merge conflicts for landing page

--- frontend/SettleCAN-Client/src/main.jsx
+++ frontend/SettleCAN-Client/src/main.jsx
@@ -1,23 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import TopNavbar from "./navigation/top-navbar.jsx";
-import App from './App.jsx'
-import Login from './register/Login.jsx';
-import Register from './register/Register.jsx';
-import ImmigrationDetails from './register/ImmigrationDetails.jsx';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import TopNavbar from "./navigation/top-navbar.jsx";
+import App from "./App.jsx";
+import Login from "./register/Login.jsx";
+import Register from "./register/Register.jsx";
+import ImmigrationDetails from "./register/ImmigrationDetails.jsx";
+import AboutPage from "./pages/Aboutpage.jsx";
+
+import "./scss/App.scss";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-        <TopNavbar />
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/immigration" element={<ImmigrationDetails />} />
-        </Routes>
+      <TopNavbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/immigration" element={<ImmigrationDetails />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
