e0940616af59b9ba9318a34cff7f4d1a892e1699
Qry <qryseymour@gmail.com>
2026-05-29 18:17:08 -0400

    Fail form if empty

--- frontend/SettleCAN-Client/src/register/Register.jsx
+++ frontend/SettleCAN-Client/src/register/Register.jsx
@@ -7,6 +7,32 @@ import { useNavigate } from 'react-router-dom';
 function ImmigrationDetails() {
     const navigate = useNavigate()
 
+    async function handleFormSubmit(formData) {
+        await formData
+        if (!formData.get('firstName')) {
+            alert('ERROR: First name is empty.')
+            return;
+        }
+        if (!formData.get('lastName')) {
+            alert('ERROR: Last name is empty.')
+            return;
+        }
+        if (!formData.get('emailphone')) {
+            alert('ERROR: Email or phone number is empty.')
+            return;
+        }
+        if (!formData.get('dateofbirth')) {
+            alert('ERROR: Date of birth is empty.')
+            return;
+        }
+        if (!formData.get('password')) {
+            alert('ERROR: Password is empty.')
+            return;
+        }
+        console.log(formData)
+    }
+
+    // eslint-disable-next-line no-unused-vars
     function handleImmigrationDetails(e) {
         e.preventDefault();
         navigate('/immigration');
@@ -18,34 +44,34 @@ function ImmigrationDetails() {
             <div style={{ "font-size": "12.5px" }}>
                 Your Journey to Canada, Simplified
             </div>
-            <Form>
+            <Form action={handleFormSubmit}>
                 <div class="row" style={{'margin-top': "67px", 'padding-left': "50px",  'padding-right': "50px", "gap": "10%" }}>
                     <div class="column" style={{"text-align": "left", "inline-size": "45%"}}>
                                 <Form.Group className="mb-3" controlId="formFirstName">
                                     <Form.Label>First Name</Form.Label>
-                                    <Form.Control type="name" style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control type="name" name="firstName" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formEmailPhone">
                                     <Form.Label>Email or Phone Number</Form.Label>
-                                    <Form.Control style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control name="emailphone" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control type="password" name="password" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                     </div>
                     <div class="column" style={{ "text-align": "left", "inline-size": "45%" }}>
                                 <Form.Group className="mb-3" controlId="formLastName">
                                     <Form.Label>Last Name</Form.Label>
-                                    <Form.Control type="name" style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control type="name" name="lastName" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formDateOfBirth">
                                     <Form.Label>Date of Birth</Form.Label>
-                                    <Form.Control type="date" style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control type="date" name="dateOfBirth" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formPasswordConfirm">
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" style={{"background": "#F8FBFF"}}/>
+                                    <Form.Control type="password" name="confirmPassword" style={{"background": "#F8FBFF"}}/>
                                 </Form.Group>
                         </div>
                     </div>
@@ -53,7 +79,7 @@ function ImmigrationDetails() {
                 <br/>
                 <br/>
                 <section id="center">
-                    <Button variant="danger" type="submit" style={{"--bs-btn-bg": "#830C10", "display": "block", "width": "67%"}} onClick={handleImmigrationDetails}>
+                    <Button variant="danger" type="submit" style={{"--bs-btn-bg": "#830C10", "display": "block", "width": "67%"}}>
                         Immigration Details
                     </Button>
                     <div>Have an account? <Link to="/login">Log in</Link></div>
