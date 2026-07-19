a58f9fcee28f7782604737b2d973f8f5a3497e7d
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2026-07-19 13:09:38 -0400

    updated variables to use hardcoded input

--- frontend/SettleCAN-Client/entrypoint.sh
+++ frontend/SettleCAN-Client/entrypoint.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-
-# 1. Substitute variables and create the config
-envsubst '${PORT} ${BACKEND_URL}' < /tmp/nginx.conf.template > /tmp/default.conf
-
-# 2. Check if Nginx config is valid
-nginx -t -c /tmp/default.conf
-
-if [ $? -eq 0 ]; then
-    # 3. Start Nginx
-    exec nginx -c /tmp/default.conf -g 'daemon off;'
-else
-    echo "Nginx configuration test failed!"
-    exit 1
-fi
+# Simply start Nginx with the static config
+exec nginx -c /etc/nginx/nginx.conf -g 'daemon off; error_log /dev/stderr info;'
