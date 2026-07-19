ccf4d863ac24db99b709f0dc1ffe2bb29b02c9ac
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2026-07-19 12:52:37 -0400

    added degault values for the variable needed by entrypoint.sh

--- frontend/SettleCAN-Client/entrypoint.sh
+++ frontend/SettleCAN-Client/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# Substitute variable and save to a file the nginx user can read
-envsubst '${BACKEND_URL}' < /tmp/nginx.conf.template > /tmp/nginx.conf
-# Start Nginx using the generated config
-nginx -c /tmp/nginx.conf -g 'daemon off;'
+
+# Use envsubst to replace variables and output to /tmp/nginx.conf
+# We must explicitly list the variables to prevent envsubst from 
+# replacing other special Nginx characters like $uri or $host
+envsubst '${PORT} ${BACKEND_URL}' < /tmp/nginx.conf.template > /tmp/nginx.conf
+
+# Start Nginx using the generated config file
+# Ensure the nginx user has read access to /tmp/nginx.conf
+exec nginx -c /tmp/nginx.conf -g 'daemon off;'
